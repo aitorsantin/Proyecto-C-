02cc003f6ec7ccfbf8c74a41734e84d21efe218b
aitorsantin <39133775+aitorsantin@users.noreply.github.com>
2020-04-23 09:48:07 +0200

    Delete pr_P_CabeceraPedido.sql

--- Procedimientos/pr_P_CabeceraPedido.sql
+++
@@ -1,9 +0,0 @@
-ALTER PROCEDURE pr_P_CabeceraPedido
-@p_codempleado tinyint, @p_codcli char(6)
-AS
-BEGIN
-	select CodPedido, Fecha, Estado, Pagado
-	from proyecto.CabeceraPedido 
-	where CodEmpleado = @p_codempleado AND CodCliente = @p_codcli 
-END
-
